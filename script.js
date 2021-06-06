2f6b18590a1ca6cd38f310553bb3d90a7da6b93b
Guilherme Datilio Ribeiro <guilhermedatilio@ibm.com>
2021-06-06 20:05:29 -0300

    Prettier formatter

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const filterInput = document.querySelector("#filterResult");
-const ui = new UI;
+const filterInput = document.querySelector('#filterResult');
+const ui = new UI();
 
 document.querySelector('.search').addEventListener('click', getCity);
 document.querySelector('#printBtn').addEventListener('click', printData);
 document.querySelector('#loading').style.display = 'none';
-document.querySelector(".error").style.display = 'none'
-document.querySelector(".infoSearch").style.display = 'none'
+document.querySelector('.error').style.display = 'none';
+document.querySelector('.infoSearch').style.display = 'none';
 
 async function getCity(e) {
   document.getElementById('loading').style.display = 'block';
@@ -14,12 +14,14 @@ async function getCity(e) {
   const year = document.querySelector('#cityYear').value;
   const month = document.querySelector('#cityMonth').value;
   const info = document.querySelector('#cityInfo').value;
-  const nameSpace = name.replaceAll(" ", "-")
-  const filterName = nameSpace.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  const nameSpace = name.replaceAll(' ', '-');
+  const filterName = nameSpace.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
 
-  await fetch(`https://transparencia.tce.sp.gov.br/api/json/${info}/${filterName}/${year}/${month}`)
-    .then(res => res.json())
-    .then(data => {
+  await fetch(
+    `https://transparencia.tce.sp.gov.br/api/json/${info}/${filterName}/${year}/${month}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
       let theadOutput = '';
 
       if (info === 'despesas') {
@@ -30,26 +32,22 @@ async function getCity(e) {
         ui.showReceita(data);
       }
 
-      document.querySelector("#dataTableHead").innerHTML = theadOutput;
-      document.querySelector('#orgao').textContent = data[0].orgao
-      document.querySelector('#relatorios').textContent = data.length
-      document.querySelector(".infoSearch").style.display = 'block'
+      document.querySelector('#dataTableHead').innerHTML = theadOutput;
+      document.querySelector('#orgao').textContent = data[0].orgao;
+      document.querySelector('#relatorios').textContent = data.length;
+      document.querySelector('.infoSearch').style.display = 'block';
       document.querySelector('#loading').style.display = 'none';
-
     })
-    .catch(err => {
-      console.log("catch error", err)
-      document.querySelector(".error").style.display = 'block'
+    .catch((err) => {
+      console.log('catch error', err);
+      document.querySelector('.error').style.display = 'block';
       setTimeout(() => {
-        document.querySelector(".error").style.display = 'none'
+        document.querySelector('.error').style.display = 'none';
       }, 3000);
       document.querySelector('#loading').style.display = 'none';
       document.querySelector('.infoSearch').style.display = 'none';
       document.querySelector('#dataTableHead').style.display = 'none';
       document.querySelector('#tableInfo_wrapper').style.display = 'none';
-
-
-
     });
   $(document).ready(function () {
     $('#tableInfo').DataTable({
@@ -64,30 +62,27 @@ async function getCity(e) {
   e.preventDefault();
 }
 
-
 $(document).ready(function () {
   fetch(`https://transparencia.tce.sp.gov.br/api/json/municipios`)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data)
-      ui.showCities(data)
-
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      ui.showCities(data);
     })
-    .catch(err => {
-      console.log("catch error", err)
-
+    .catch((err) => {
+      console.log('catch error', err);
     });
 });
 
-
 function printData() {
-  var divToPrint = document.getElementById("tableInfo");
-  var divToPrint2 = document.getElementById("cityQtd");
-  newWin = window.open("");
-  newWin.document.write('<link rel="stylesheet" href="src/css/style.css" type="text/css" />');
+  var divToPrint = document.getElementById('tableInfo');
+  var divToPrint2 = document.getElementById('cityQtd');
+  newWin = window.open('');
+  newWin.document.write(
+    '<link rel="stylesheet" href="src/css/style.css" type="text/css" />'
+  );
   newWin.document.write(divToPrint2.outerHTML);
   newWin.document.write(divToPrint.outerHTML);
   newWin.print();
   newWin.close();
 }
-
--- ui.js
+++ ui.js
@@ -6,7 +6,7 @@ const theadDespesa = `<tr>
   <th class="text-center" scope="col">Nome do fornecedor</th>
   <th class="text-center" scope="col">Data</th>
   <th class="text-center" scope="col">Valor da despesa</th>
-</tr>`
+</tr>`;
 
 const theadReceita = `<tr>
   <th class="text-center" scope="col">#</th>
@@ -16,15 +16,14 @@ const theadReceita = `<tr>
   <th class="text-center" scope="col">Código e descrição da Subalínea</th>
   <th class="text-center" scope="col">Mês</th>
   <th class="text-center" scope="col">Valor da arrecadação</th>
-</tr>`
+</tr>`;
 
 class UI {
-
   showDespesa(data) {
     let tbodyOutput = '';
     let i = 0;
     data.forEach(function (city) {
-      i++
+      i++;
       tbodyOutput += `
       <tr id=${i}>
         <td class="text-center">${i}</td>
@@ -35,15 +34,15 @@ class UI {
         <td class="text-center">${city.dt_emissao_despesa}</td>
         <td class="text-center">${city.vl_despesa}</td>
       </tr>
-    `
-    })
-    document.querySelector("#dataTable").innerHTML = tbodyOutput;
+    `;
+    });
+    document.querySelector('#dataTable').innerHTML = tbodyOutput;
   }
   showReceita(data) {
     let tbodyOutput = '';
     let i = 0;
     data.forEach(function (city) {
-      i++
+      i++;
       tbodyOutput += `
       <tr id=${i}>
         <td class="text-center">${i}</td>
@@ -54,205 +53,205 @@ class UI {
         <td class="text-center">${city.mes}</td>
         <td class="text-center">${city.vl_arrecadacao}</td>
       </tr>
-    `
-    })
-    document.querySelector("#dataTable").innerHTML = tbodyOutput;
+    `;
+    });
+    document.querySelector('#dataTable').innerHTML = tbodyOutput;
   }
   showCities(data) {
     let modalBody = '';
     let i = 0;
     data.forEach(function (city) {
-      i++
+      i++;
       modalBody += `<tr id=${i}>
         <td class="text-center">${i}</td>
         <td class="text-center">${city.municipio_extenso}</td>
-      </tr>`
-    })
-    document.querySelector("#modalCities").innerHTML = modalBody;
+      </tr>`;
+    });
+    document.querySelector('#modalCities').innerHTML = modalBody;
   }
 }
 
 const dataTableTranslation = {
-  "emptyTable": "Nenhum registro encontrado",
-  "info": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
-  "infoEmpty": "Mostrando 0 até 0 de 0 registros",
-  "infoFiltered": "(Filtrados de _MAX_ registros)",
-  "infoThousands": ".",
-  "loadingRecords": "Carregando...",
-  "processing": "Processando...",
-  "zeroRecords": "Nenhum registro encontrado",
-  "search": "Pesquisar",
-  "paginate": {
-    "next": "Próximo",
-    "previous": "Anterior",
-    "first": "Primeiro",
-    "last": "Último"
+  emptyTable: 'Nenhum registro encontrado',
+  info: 'Mostrando de _START_ até _END_ de _TOTAL_ registros',
+  infoEmpty: 'Mostrando 0 até 0 de 0 registros',
+  infoFiltered: '(Filtrados de _MAX_ registros)',
+  infoThousands: '.',
+  loadingRecords: 'Carregando...',
+  processing: 'Processando...',
+  zeroRecords: 'Nenhum registro encontrado',
+  search: 'Pesquisar',
+  paginate: {
+    next: 'Próximo',
+    previous: 'Anterior',
+    first: 'Primeiro',
+    last: 'Último',
   },
-  "aria": {
-    "sortAscending": ": Ordenar colunas de forma ascendente",
-    "sortDescending": ": Ordenar colunas de forma descendente"
+  aria: {
+    sortAscending: ': Ordenar colunas de forma ascendente',
+    sortDescending: ': Ordenar colunas de forma descendente',
   },
-  "select": {
-    "rows": {
-      "_": "Selecionado %d linhas",
-      "0": "Nenhuma linha selecionada",
-      "1": "Selecionado 1 linha"
+  select: {
+    rows: {
+      _: 'Selecionado %d linhas',
+      0: 'Nenhuma linha selecionada',
+      1: 'Selecionado 1 linha',
     },
-    "1": "%d linha selecionada",
-    "_": "%d linhas selecionadas",
-    "cells": {
-      "1": "1 célula selecionada",
-      "_": "%d células selecionadas"
+    1: '%d linha selecionada',
+    _: '%d linhas selecionadas',
+    cells: {
+      1: '1 célula selecionada',
+      _: '%d células selecionadas',
     },
-    "columns": {
-      "1": "1 coluna selecionada",
-      "_": "%d colunas selecionadas"
-    }
+    columns: {
+      1: '1 coluna selecionada',
+      _: '%d colunas selecionadas',
+    },
   },
-  "buttons": {
-    "copySuccess": {
-      "1": "Uma linha copiada com sucesso",
-      "_": "%d linhas copiadas com sucesso"
+  buttons: {
+    copySuccess: {
+      1: 'Uma linha copiada com sucesso',
+      _: '%d linhas copiadas com sucesso',
     },
-    "collection": "Coleção  <span class=\"ui-button-icon-primary ui-icon ui-icon-triangle-1-s\"><\/span>",
-    "colvis": "Visibilidade da Coluna",
-    "colvisRestore": "Restaurar Visibilidade",
-    "copy": "Copiar",
-    "copyKeys": "Pressione ctrl ou u2318 + C para copiar os dados da tabela para a área de transferência do sistema. Para cancelar, clique nesta mensagem ou pressione Esc..",
-    "copyTitle": "Copiar para a Área de Transferência",
-    "csv": "CSV",
-    "excel": "Excel",
-    "pageLength": {
-      "-1": "Mostrar todos os registros",
-      "1": "Mostrar 1 registro",
-      "_": "Mostrar %d registros"
+    collection:
+      'Coleção  <span class="ui-button-icon-primary ui-icon ui-icon-triangle-1-s"></span>',
+    colvis: 'Visibilidade da Coluna',
+    colvisRestore: 'Restaurar Visibilidade',
+    copy: 'Copiar',
+    copyKeys:
+      'Pressione ctrl ou u2318 + C para copiar os dados da tabela para a área de transferência do sistema. Para cancelar, clique nesta mensagem ou pressione Esc..',
+    copyTitle: 'Copiar para a Área de Transferência',
+    csv: 'CSV',
+    excel: 'Excel',
+    pageLength: {
+      '-1': 'Mostrar todos os registros',
+      1: 'Mostrar 1 registro',
+      _: 'Mostrar %d registros',
     },
-    "pdf": "PDF",
-    "print": "Imprimir"
+    pdf: 'PDF',
+    print: 'Imprimir',
   },
-  "autoFill": {
-    "cancel": "Cancelar",
-    "fill": "Preencher todas as células com",
-    "fillHorizontal": "Preencher células horizontalmente",
-    "fillVertical": "Preencher células verticalmente"
+  autoFill: {
+    cancel: 'Cancelar',
+    fill: 'Preencher todas as células com',
+    fillHorizontal: 'Preencher células horizontalmente',
+    fillVertical: 'Preencher células verticalmente',
   },
-  "lengthMenu": "Exibir _MENU_ resultados por página",
-  "searchBuilder": {
-    "add": "Adicionar Condição",
-    "button": {
-      "0": "Construtor de Pesquisa",
-      "_": "Construtor de Pesquisa (%d)"
+  lengthMenu: 'Exibir _MENU_ resultados por página',
+  searchBuilder: {
+    add: 'Adicionar Condição',
+    button: {
+      0: 'Construtor de Pesquisa',
+      _: 'Construtor de Pesquisa (%d)',
     },
-    "clearAll": "Limpar Tudo",
-    "condition": "Condição",
-    "conditions": {
-      "date": {
-        "after": "Depois",
-        "before": "Antes",
-        "between": "Entre",
-        "empty": "Vazio",
-        "equals": "Igual",
-        "not": "Não",
-        "notBetween": "Não Entre",
-        "notEmpty": "Não Vazio"
+    clearAll: 'Limpar Tudo',
+    condition: 'Condição',
+    conditions: {
+      date: {
+        after: 'Depois',
+        before: 'Antes',
+        between: 'Entre',
+        empty: 'Vazio',
+        equals: 'Igual',
+        not: 'Não',
+        notBetween: 'Não Entre',
+        notEmpty: 'Não Vazio',
       },
-      "number": {
-        "between": "Entre",
-        "empty": "Vazio",
-        "equals": "Igual",
-        "gt": "Maior Que",
-        "gte": "Maior ou Igual a",
-        "lt": "Menor Que",
-        "lte": "Menor ou Igual a",
-        "not": "Não",
-        "notBetween": "Não Entre",
-        "notEmpty": "Não Vazio"
+      number: {
+        between: 'Entre',
+        empty: 'Vazio',
+        equals: 'Igual',
+        gt: 'Maior Que',
+        gte: 'Maior ou Igual a',
+        lt: 'Menor Que',
+        lte: 'Menor ou Igual a',
+        not: 'Não',
+        notBetween: 'Não Entre',
+        notEmpty: 'Não Vazio',
       },
-      "string": {
-        "contains": "Contém",
-        "empty": "Vazio",
-        "endsWith": "Termina Com",
-        "equals": "Igual",
-        "not": "Não",
-        "notEmpty": "Não Vazio",
-        "startsWith": "Começa Com"
+      string: {
+        contains: 'Contém',
+        empty: 'Vazio',
+        endsWith: 'Termina Com',
+        equals: 'Igual',
+        not: 'Não',
+        notEmpty: 'Não Vazio',
+        startsWith: 'Começa Com',
       },
-      "array": {
-        "contains": "Contém",
-        "empty": "Vazio",
-        "equals": "Igual à",
-        "not": "Não",
-        "notEmpty": "Não vazio",
-        "without": "Não possui"
-      }
-    },
-    "data": "Data",
-    "deleteTitle": "Excluir regra de filtragem",
-    "logicAnd": "E",
-    "logicOr": "Ou",
-    "title": {
-      "0": "Construtor de Pesquisa",
-      "_": "Construtor de Pesquisa (%d)"
-    },
-    "value": "Valor"
-  },
-  "searchPanes": {
-    "clearMessage": "Limpar Tudo",
-    "collapse": {
-      "0": "Painéis de Pesquisa",
-      "_": "Painéis de Pesquisa (%d)"
-    },
-    "count": "{total}",
-    "countFiltered": "{shown} ({total})",
-    "emptyPanes": "Nenhum Painel de Pesquisa",
-    "loadMessage": "Carregando Painéis de Pesquisa...",
-    "title": "Filtros Ativos"
-  },
-  "searchPlaceholder": "Digite um termo para pesquisar",
-  "thousands": ".",
-  "datetime": {
-    "previous": "Anterior",
-    "next": "Próximo",
-    "hours": "Hora",
-    "minutes": "Minuto",
-    "seconds": "Segundo",
-    "amPm": [
-      "am",
-      "pm"
-    ],
-    "unknown": "-"
-  },
-  "editor": {
-    "close": "Fechar",
-    "create": {
-      "button": "Novo",
-      "submit": "Criar",
-      "title": "Criar novo registro"
-    },
-    "edit": {
-      "button": "Editar",
-      "submit": "Atualizar",
-      "title": "Editar registro"
-    },
-    "error": {
-      "system": "Ocorreu um erro no sistema (<a target=\"\\\" rel=\"nofollow\" href=\"\\\">Mais informações<\/a>)."
-    },
-    "multi": {
-      "noMulti": "Essa entrada pode ser editada individualmente, mas não como parte do grupo",
-      "restore": "Desfazer alterações",
-      "title": "Multiplos valores",
-      "info": "Os itens selecionados contêm valores diferentes para esta entrada. Para editar e definir todos os itens para esta entrada com o mesmo valor, clique ou toque aqui, caso contrário, eles manterão seus valores individuais."
-    },
-    "remove": {
-      "button": "Remover",
-      "confirm": {
-        "_": "Tem certeza que quer deletar %d linhas?",
-        "1": "Tem certeza que quer deletar 1 linha?"
+      array: {
+        contains: 'Contém',
+        empty: 'Vazio',
+        equals: 'Igual à',
+        not: 'Não',
+        notEmpty: 'Não vazio',
+        without: 'Não possui',
       },
-      "submit": "Remover",
-      "title": "Remover registro"
-    }
+    },
+    data: 'Data',
+    deleteTitle: 'Excluir regra de filtragem',
+    logicAnd: 'E',
+    logicOr: 'Ou',
+    title: {
+      0: 'Construtor de Pesquisa',
+      _: 'Construtor de Pesquisa (%d)',
+    },
+    value: 'Valor',
   },
-  "decimal": ","
-}
-
+  searchPanes: {
+    clearMessage: 'Limpar Tudo',
+    collapse: {
+      0: 'Painéis de Pesquisa',
+      _: 'Painéis de Pesquisa (%d)',
+    },
+    count: '{total}',
+    countFiltered: '{shown} ({total})',
+    emptyPanes: 'Nenhum Painel de Pesquisa',
+    loadMessage: 'Carregando Painéis de Pesquisa...',
+    title: 'Filtros Ativos',
+  },
+  searchPlaceholder: 'Digite um termo para pesquisar',
+  thousands: '.',
+  datetime: {
+    previous: 'Anterior',
+    next: 'Próximo',
+    hours: 'Hora',
+    minutes: 'Minuto',
+    seconds: 'Segundo',
+    amPm: ['am', 'pm'],
+    unknown: '-',
+  },
+  editor: {
+    close: 'Fechar',
+    create: {
+      button: 'Novo',
+      submit: 'Criar',
+      title: 'Criar novo registro',
+    },
+    edit: {
+      button: 'Editar',
+      submit: 'Atualizar',
+      title: 'Editar registro',
+    },
+    error: {
+      system:
+        'Ocorreu um erro no sistema (<a target="\\" rel="nofollow" href="\\">Mais informações</a>).',
+    },
+    multi: {
+      noMulti:
+        'Essa entrada pode ser editada individualmente, mas não como parte do grupo',
+      restore: 'Desfazer alterações',
+      title: 'Multiplos valores',
+      info: 'Os itens selecionados contêm valores diferentes para esta entrada. Para editar e definir todos os itens para esta entrada com o mesmo valor, clique ou toque aqui, caso contrário, eles manterão seus valores individuais.',
+    },
+    remove: {
+      button: 'Remover',
+      confirm: {
+        _: 'Tem certeza que quer deletar %d linhas?',
+        1: 'Tem certeza que quer deletar 1 linha?',
+      },
+      submit: 'Remover',
+      title: 'Remover registro',
+    },
+  },
+  decimal: ',',
+};
